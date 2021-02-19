3d8aecf6b5526501b8699e1da060442153a4b6f7
lazycoder2021 <tksingh2087@gmail.com>
2021-02-19 13:20:08 +0530

    initial committ

---
+++ src/components/ui/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'; 
+import logo from '../../img/logo.png'; 
+
+
+const Header = () => {
+    return (
+        <header className="center">
+            <img src={logo} alt='' />
+        </header>
+    )
+
+}
+
+export default Header; 
+
+
+
+
+
+
+
+// JavaScript source code
---
+++ src/components/ui/Search.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'; 
+
+const Search = ({ getQuery }) => {
+    const [text, setText] = useState(''); 
+
+    const onChange = (q) => {
+        setText(q)
+        getQuery(q)
+    }
+
+    return (
+        
+           <section className="search">
+            <form>
+                <input type="text" placeholder="Search Characters" className='form-control' autofocus value={text} onChange={(e) => onChange(e.target.value)} />
+            </form>
+           </section>
+
+        
+    )
+
+}
+
+export default Search; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// JavaScript source code
